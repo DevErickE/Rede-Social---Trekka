7ebda102fdc02c51b62341ff1d19bd31cd5034c3
DevErickE <erickmarcos.ferreira003@gmail.com>
2025-04-04 16:15:49 -0300

    Update - adicionando função aos  botões

---
+++ script.js
@@ -0,0 +1,8 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".imagem-tenis")
+function mudarVisual(cor, imagem) {
+
+    body.style.background = cor
+    tenis.src = imagem
+
+}
